d81e7f3d9a65b6cd61c06babacff47ef580f7c1c
mbaibatyr <murat_b@mail.ru>
2024-06-04 21:44:35 +0500

    --

---
+++ Call/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Call
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("C:\\Users\\байбатыровм\\source\\repos\\SEP_221_SYSTEM\\MyProcess\\bin\\Debug\\net6.0\\MyProcess.exe");
+        }
+    }
+}
--- MyProcess/Program.cs
+++ MyProcess/Program.cs
@@ -1,23 +1,39 @@
-﻿namespace MyProcess
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace MyProcess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("кол-во параметров должно быть = 2");
-                return;
-            }
-            var result =  int.Parse(args[0]) + int.Parse(args[1]);
-            if(Check(args[0]) == -1000000)
-            {
+            StartProcess(@"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE");
+            //List<Process> lst = new List<Process>();
+            //lst.Add(StartProcess("calc"));
+            //lst.Add(StartProcess("calc"));
+            //lst.Add(StartProcess("calc"));
+            //foreach (Process p in lst) 
+            //{ 
+            //    p.Kill();
+            //}
 
-            }
-            Console.WriteLine(result);
+            //KillProcessesByName("CalculatorApp");
+
+            //GetAllProcesses();
+
+            //if (args.Length != 2)
+            //{
+            //    Console.WriteLine("кол-во параметров должно быть = 2");
+            //    return;
+            //}
+            //var result =  int.Parse(args[0]) + int.Parse(args[1]);
+            //if(Check(args[0]) == -1000000)
+            //{
+
+            //}
+            //Console.WriteLine(result);
             //Console.ReadLine();
         }
-
         static int Check(string st)
         {
             try
@@ -28,6 +44,30 @@
             {
                 return -1000000;
             }
+        }
+
+        static void GetAllProcesses()
+        {
+            foreach (var item in Process.GetProcesses().Where(z => z.ProcessName == "chrome"))
+            {
+                //if (item.ProcessName == "chrome")
+                //Console.WriteLine($"{item.Id}  {item.ProcessName}  {item.BasePriority}");
+                //item.Kill();
+            }
+        }
+
+        static void KillProcessesByName(string Name)
+        {
+            foreach (var item in Process.GetProcessesByName(Name))
+            {
+                item.Kill();
+            }
+        }
+
+        static Process StartProcess(string Name)
+        {
+            var p = Process.Start(Name);            
+            return p as Process;
         }
     }
 }
