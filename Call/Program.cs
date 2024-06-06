e23211afaa361a3916535a22f20f20783eb32e6c
mbaibatyr <murat_b@mail.ru>
2024-06-06 20:12:33 +0500

    --

--- Call/Program.cs
+++ Call/Program.cs
@@ -12,9 +12,15 @@ namespace Call
             info.FileName = "C:\\Users\\байбатыровм\\source\\repos\\SEP_221_SYSTEM\\MyProcess\\bin\\Debug\\net6.0\\MyProcess.exe";
             info.Arguments = "50 120";
             info.WindowStyle = ProcessWindowStyle.Normal;
-            //info.UseShellExecute = false;
-            //info.CreateNoWindow = true;            
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
             var p = Process.Start(info);
+            while (!p.StandardOutput.EndOfStream)
+            {
+                string line = p.StandardOutput.ReadLine();
+                Console.WriteLine($"Результат {line}");
+            }
             p.WaitForExit();
 
         }
