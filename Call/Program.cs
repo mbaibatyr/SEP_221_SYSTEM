980755404bae14f8757c5cb55cc6288af08f1582
mbaibatyr <murat_b@mail.ru>
2024-06-06 21:42:33 +0500

    --

--- Call/Program.cs
+++ Call/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using MyDLL;
 
 namespace Call
 {
@@ -6,22 +7,27 @@ namespace Call
     {
         static void Main(string[] args)
         {
+            MyClass myClass = new MyClass();
+            var result = myClass.Sum(50, 20);
+            Console.WriteLine(result);
+            Console.WriteLine(myClass.Hello("STEP"));
+
             //Process.Start("C:\\Users\\байбатыровм\\source\\repos\\SEP_221_SYSTEM\\MyProcess\\bin\\Debug\\net6.0\\MyProcess.exe");
 
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.FileName = "C:\\Users\\байбатыровм\\source\\repos\\SEP_221_SYSTEM\\MyProcess\\bin\\Debug\\net6.0\\MyProcess.exe";
-            info.Arguments = "50 120";
-            info.WindowStyle = ProcessWindowStyle.Normal;
-            info.UseShellExecute = false;
-            info.CreateNoWindow = true;
-            info.RedirectStandardOutput = true;
-            var p = Process.Start(info);
-            while (!p.StandardOutput.EndOfStream)
-            {
-                string line = p.StandardOutput.ReadLine();
-                Console.WriteLine($"Результат {line}");
-            }
-            p.WaitForExit();
+            //ProcessStartInfo info = new ProcessStartInfo();
+            //info.FileName = "C:\\Users\\байбатыровм\\source\\repos\\SEP_221_SYSTEM\\MyProcess\\bin\\Debug\\net6.0\\MyProcess.exe";
+            //info.Arguments = "50 120";
+            //info.WindowStyle = ProcessWindowStyle.Normal;
+            //info.UseShellExecute = false;
+            //info.CreateNoWindow = true;
+            //info.RedirectStandardOutput = true;
+            //var p = Process.Start(info);
+            //while (!p.StandardOutput.EndOfStream)
+            //{
+            //    string line = p.StandardOutput.ReadLine();
+            //    Console.WriteLine($"Результат {line}");
+            //}
+            //p.WaitForExit();
 
         }
     }
---
+++ MyDLL/MyClass.cs
@@ -0,0 +1,12 @@
+﻿namespace MyDLL
+{
+    public class MyClass
+    {
+        public int Sum(int a, int b) 
+        { 
+            return a + b;
+        }
+
+        public string Hello(string name) => "Hello " + name;
+    }
+}
