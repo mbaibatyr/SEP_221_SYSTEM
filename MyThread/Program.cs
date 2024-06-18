81cc58052fc8b3764ae409e2f1f1a699fa75fac6
mbaibatyr <murat_b@mail.ru>
2024-06-18 19:52:45 +0500

    --

--- MyThread/Program.cs
+++ MyThread/Program.cs
@@ -6,28 +6,29 @@ namespace MyThread
     {
         static void Main(string[] args)
         {
-            DateTime dt1 = DateTime.Now;
-            DateTime dt2 = DateTime.Now.AddMinutes(2);
-            Console.WriteLine((dt2 - dt1).TotalSeconds);
+            //DateTime dt1 = DateTime.Now;
+            //DateTime dt2 = DateTime.Now.AddMinutes(2);
+            //Console.WriteLine((dt2 - dt1).TotalSeconds);
 
             //Thread thread = Thread.CurrentThread;
             //thread.Priority = ThreadPriority.Highest;
             //Console.WriteLine($"{thread.Priority}");
 
-            //for (int i = 1; i <= 5; i++)
-            //{
-            //    //Thread MyThread = new Thread(Test);
-            //    Thread MyThread = new Thread (new ParameterizedThreadStart(Test2));
-            //    MyThread.Name = "Поток " + i.ToString();
-            //    MyThread.Start(i);
-            //}
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread MyThread = new Thread(Test);
+                //Thread MyThread = new Thread(new ParameterizedThreadStart(Test2));
+                MyThread.Name = "Поток " + i.ToString();
+                //MyThread.Start(i);
+                MyThread.Start();
+            }
 
             //Console.ReadLine();
         }
 
         static void Test()
         {
-            for (int i = 0; i < 10; i++) 
+            for (int i = 0; i < 10; i++)
             {
                 Thread thread = Thread.CurrentThread;
                 Console.WriteLine($"{thread.Name} - {i}");
