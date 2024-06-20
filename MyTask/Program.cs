58da58f8264c67b9898785b34d86d05e1e554ddf
mbaibatyr <murat_b@mail.ru>
2024-06-20 19:16:03 +0500

    ---

--- MyTask/Program.cs
+++ MyTask/Program.cs
@@ -14,6 +14,7 @@
 
             string[] list = new string[] { "1", "2", "3", "3" };
             list = list.Distinct().ToArray();
+            
 
             //using (Sqlconnection db = new Sqlconnection(""))
             //{
