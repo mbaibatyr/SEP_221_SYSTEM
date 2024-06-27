e549e17136597ebe4824a6a23053a66b66a8485d
mbaibatyr <murat_b@mail.ru>
2024-06-27 20:23:56 +0500

    --

---
+++ MyTimer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Timers;
+
+namespace MyTimer
+{
+    internal class Program
+    {
+        System.Threading.Timer timer1;
+
+        static void Main(string[] args)
+        {
+            MyTimer1 myTimer1 = new MyTimer1();
+            Console.ReadLine();
+        }
+    }
+
+    class MyTimer1
+    {
+        System.Timers.Timer timer;
+        System.Timers.Timer timer2;
+        public MyTimer1()
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 500;
+            timer.Elapsed += ShowConsole;
+            //timer.Start();
+
+            timer2 = new System.Timers.Timer();
+            timer2.Interval = 3000;
+            timer2.Elapsed += SWitchConsole;
+            timer2.Start();
+        }
+
+        public void ShowConsole(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Hello step");
+        }
+
+        public void SWitchConsole(object sender, ElapsedEventArgs e)
+        {
+            timer.Enabled = !timer.Enabled;
+        }
+    }
+}
