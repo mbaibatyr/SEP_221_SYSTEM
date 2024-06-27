adec95551084e5332d43382fe5dc8f2eea8b3bfa
mbaibatyr <murat_b@mail.ru>
2024-06-27 20:47:22 +0500

    --

--- MyTimer/Program.cs
+++ MyTimer/Program.cs
@@ -1,17 +1,29 @@
 ﻿using System.Diagnostics.Metrics;
 using System.Reflection;
+using System.Threading;
 using System.Timers;
 
 namespace MyTimer
 {
     internal class Program
     {
-        System.Threading.Timer timer1;
+        static System.Threading.Timer timer1;
 
         static void Main(string[] args)
         {
-            MyTimer1 myTimer1 = new MyTimer1();
+            timer1 = new System.Threading.Timer(ShowConsole, null, 0, 1000);
+            Thread.Sleep(2000);
+            timer1.Change(1000, 0);
+            Thread.Sleep(2000);
+            timer1.Change(1000, 100);
+            Thread.Sleep(2000);
+            timer1.Change(1000, 1000);
+            //MyTimer1 myTimer1 = new MyTimer1();
             Console.ReadLine();
+        }
+        static void ShowConsole(object sender)
+        {
+            Console.WriteLine("Hello step");
         }
     }
 
